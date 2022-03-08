f88601ea832927e08e799d39f10c4574af4841a6
Hailey Thomas <haileyraebeauty@gmail.com>
2022-03-07 18:53:54 -0700

    create Campaign schema

---
+++ server/models/Campaign.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require("mongoose");
+
+const campaignSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Campaign title is required!"],
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required!"],
+      trim: true,
+    },
+    image: {
+      type: String,
+      trim: true,
+    },
+    posts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+const Campaign = model("Campaign", campaignSchema);
+
+module.exports = Campaign;
--- server/models/index.js
+++ server/models/index.js
@@ -3,5 +3,6 @@ const SocialMedia = require("./SocialMedia");
 const Product = require("./Product");
 const Ad = require("./Ad");
 const Look = require("./Look");
+const Campaign = require("./Campaign");
 
-module.exports = { Owner, Affiliate, SocialMedia, Product, Ad, Look };
+module.exports = { Owner, Affiliate, SocialMedia, Product, Ad, Look, Campaign };
