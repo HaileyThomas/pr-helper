281d3c9b6a13dce53900b69730471176d0eaf62e
Hailey Thomas <haileyraebeauty@gmail.com>
2022-03-16 16:40:51 -0700

    create brand queries

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -25,5 +25,28 @@ const resolvers = {
       }
       throw new AuthenticationError("You are not logged in!");
     },
+    // get user's brands
+    myBrands: async (_, __, context) => {
+      if (context.user) {
+        const { brands } = await User.findById(context.user._id)
+          .select("brands")
+          .populate("brands");
+        return brands;
+      }
+      throw new AuthenticationError("Not logged in!");
+    },
+    // get a brand by id
+    brand: async (_, { _id }, context) => {
+      // check if user is logged in
+      if (context.user) {
+        const brandData = await Brand.findById(_id);
+        if (!brandData) {
+          throw new Error("Brand not found!");
+        }
+        return await Brand.findById(_id)
+          .populate("affiliates")
+          .populate("socials");
+      }
+    },
   },
 };
