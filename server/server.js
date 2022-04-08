f09cf9c6e4ebe0991fae43dcfd1dc712daa18537
Hailey Thomas <haileyraebeauty@gmail.com>
2022-04-08 14:51:46 -0700

    edit server to take out static code to run graphql

--- server/server.js
+++ server/server.js
@@ -26,13 +26,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // serve up static assets
-if (process.env.NODE_ENV === "production") {
+/* if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+}); */
 
 // listen for server to be running
 db.once("open", () => {
